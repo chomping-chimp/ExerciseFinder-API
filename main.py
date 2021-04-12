4204b42d069b688babedf17fdabc4836aec2ee3c
Radhe Pokhrel <rshyampokhrel@gmail.com>
2021-04-11 20:02:19 -0400

    Started Project

---
+++ main.py
@@ -0,0 +1,8 @@
+import src.processor as proc
+
+def mainrunner():
+    # c = (have ot make this the main function from the processor script)
+    c.mainloop()
+
+if __name__ == '__main__':
+    mainrunner()
---
+++ src/processor.py
@@ -0,0 +1,2 @@
+import generator as gen
+import getInput
