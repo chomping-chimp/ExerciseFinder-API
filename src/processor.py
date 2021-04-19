c54c439edd1ac8f961824997a490018815ff779a
Radhe Pokhrel <rshyampokhrel@gmail.com>
2021-04-18 23:19:25 -0400

    Linked DB up

--- src/processor.py
+++ src/processor.py
@@ -1,2 +1,17 @@
 import generator as gen
 import getInput
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="sql3.freesqldatabase.com",
+  user="sql3406581",
+  password="K1FQBr2Tr1"
+)
+mycursor = mydb.cursor()
+
+mycursor.execute("SELECT * FROM sql3406581.Exercises")
+
+myresult = mycursor.fetchall()
+
+for x in myresult:
+  print(x)
