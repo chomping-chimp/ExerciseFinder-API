c743c30389c13cd50d17edb9191e8e746b45facf
Radhe Pokhrel <rshyampokhrel@gmail.com>
2021-05-03 10:25:34 -0400

    Made GET response functional

--- api/fetcher.py
+++ api/fetcher.py
@@ -1,9 +1,8 @@
 from . import db_api
-
+import json
 db = db_api.DB()
 
 def get_all():
     result = db.db_get("select * from Fitness.exerciseList")
     print(result)
-    response = json.dumps(result)
-    return response
+    return result
--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 from bottle import request, response
 from bottle import post, get, put, delete
 from bottle import Bottle, run
+import json
 from api import fetcher
 
 app = Bottle()
 
 @app.route('/api/exercise')
 def fetch():
-    return fetcher.get_all()
+    data = fetcher.get_all()
+    response.headers['Content-Type'] = 'application/json'
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Credentials'] = 'GET, POST, OPTIONS'
+    response.headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept'
+    return json.dumps({'data': data})
 
 # @put('/names/<name>')
 # def update_handler(name):
