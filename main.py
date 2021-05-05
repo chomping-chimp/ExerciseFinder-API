d841ddabf6fa25c17663860c0109d00817672b9d
Radhe Shyam Pokhrel <52668053+chomping-chimp@users.noreply.github.com>
2021-05-04 22:01:48 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,30 +4,29 @@ from bottle import Bottle, run
 import json
 from api import fetcher
 
-app = Bottle()
+bottle = Bottle()
+def app():
+    @app.route('/api/allExercise')
+    def fetch():
+        data = fetcher.get_all()
+        response.headers['Content-Type'] = 'application/json'
+        response.headers['Access-Control-Allow-Origin'] = '*'
+        response.headers['Access-Control-Allow-Credentials'] = 'GET, POST, OPTIONS'
+        response.headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept'
+        return json.dumps({'data': data})
 
-@app.route('/api/allExercise')
-def fetch():
-    data = fetcher.get_all()
-    response.headers['Content-Type'] = 'application/json'
-    response.headers['Access-Control-Allow-Origin'] = '*'
-    response.headers['Access-Control-Allow-Credentials'] = 'GET, POST, OPTIONS'
-    response.headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept'
-    return json.dumps({'data': data})
+    # @put('/names/<name>')
+    # def update_handler(name):
+    #     '''Handles name updates'''
+    #     pass
 
-# @put('/names/<name>')
-# def update_handler(name):
-#     '''Handles name updates'''
-#     pass
+    # @delete('/names/<name>')
+    # def delete_handler(name):
+    #     '''Handles name deletions'''
+    #     pass
 
-# @delete('/names/<name>')
-# def delete_handler(name):
-#     '''Handles name deletions'''
-#     pass
-
-# @post('/names')
-# def creation_handler():
-#     '''Handles name creation'''
-#     pass
-
-run(app, host='localhost', port=8080, fast=True)
+    # @post('/names')
+    # def creation_handler():
+    #     '''Handles name creation'''
+    #     pass
+run(bottle, host='localhost', port=8080, server='gunicorn', fast=True)
