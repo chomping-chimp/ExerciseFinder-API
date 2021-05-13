72f8bf34d366538c7b12e660b214661ac8356cbb
Radhe Pokhrel <rshyampokhrel@gmail.com>
2021-05-13 16:38:44 -0400

    Added FIlter Routing

--- main.py
+++ main.py
@@ -5,6 +5,16 @@ from api import fetcher
 
 app = Flask(__name__)
 
+'''
+Testing Route to check server
+'''
+@app.route('/')
+def index():
+    return "<h1>Welcome to our server !!</h1>"
+
+'''
+Route to get all exercises 
+'''
 @app.route('/exercise/getAll', methods=['GET'])
 def respond():
     resp = make_response(json.dumps({'data': fetcher.get_all()}))
@@ -14,35 +24,20 @@ def respond():
     resp.headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept'
     return resp
 
-# @app.route('/exercise/getAll', methods=['GET'])
-# def respond():
-#     resp = make_response(json.dumps({'data': fetcher.get_all()}))
-#     resp.headers['Content-Type'] = 'application/json'
-#     resp.headers['Access-Control-Allow-Origin'] = '*'
-#     resp.headers['Access-Control-Allow-Credentials'] = 'GET, POST, OPTIONS'
-#     resp.headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept'
-#     return resp
-# Need to look into 
+'''
+Route to filter through exercises based on Muscle Group
+'''
+@app.route('/exercise/filter', methods=['GET'])
+def filterData():
+    filters = request.args.get('filters', '')
+    resp = make_response(json.dumps({'data': fetcher.filterGroups(filters)}))
+    resp.headers['Content-Type'] = 'application/json'
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    resp.headers['Access-Control-Allow-Credentials'] = 'GET, POST, OPTIONS'
+    resp.headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept'
+    return resp
 
-# @app.route('/post/', methods=['POST'])
-# def post_something():
-#     param = request.form.get('name')
-#     print(param)
-#     # You can add the test cases you made in the previous function, but in our case here you are just testing the POST functionality
-#     if param:
-#         return jsonify({
-#             "Message": f"Welcome {name} to our awesome platform!!",
-#             # Add this option to distinct the POST request
-#             "METHOD" : "POST"
-#         })
-#     else:
-#         return jsonify({
-#             "ERROR": "no name found, please send a name."
-#         })
-
-@app.route('/')
-def index():
-    return "<h1>Welcome to our server !!</h1>"
+# http://127.0.0.1:5000/exercise/filter?filters=name%impo
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
