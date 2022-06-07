612df92b9b5139eb83eaf9b8cc6213ddf5d041f7
Radhe Pokhrel <rshyampokhrel@gmail.com>
2022-06-06 20:14:50 -0400

    Ordering scoreboard

--- models/GroupModel.py
+++ models/GroupModel.py
@@ -22,31 +22,25 @@ class GroupModel(BaseModel):
 
     def get_scoreboard(self, timeframe=None):
         
-        query = ""
+        where_clause = ""
+        group_clause = ""
         if timeframe == "day":
-            query = """
-                SELECT usr.username, SUM(log.active_minutes) activity, log.create_time date
-                FROM scoreboard_log log
-                JOIN scoreboard_users usr ON usr.id = log.user_id
-                WHERE log.create_time <= NOW() AND log.create_time >= NOW() - INTERVAL 12 hour
-                GROUP BY DAY(create_time), usr.username;
-            """
+            where_clause = "WHERE log.create_time <= NOW() AND log.create_time >= NOW() - INTERVAL 12 hour"
+            group_clause = "GROUP BY DAY(create_time), usr.username"
         elif timeframe == "week":
-            query = """
-                SELECT usr.username, SUM(log.active_minutes) activity, log.create_time date
-                FROM scoreboard_log log
-                JOIN scoreboard_users usr ON usr.id = log.user_id
-                WHERE log.create_time <= NOW() AND log.create_time >= NOW() - INTERVAL 7 day
-                GROUP BY DAY(create_time), usr.username;
-            """
+            where_clause = "WHERE log.create_time <= NOW() AND log.create_time >= NOW() - INTERVAL 7 day"
+            group_clause = "GROUP BY DAY(create_time), usr.username"
         else:
-            query = """
-                SELECT usr.username, SUM(log.active_minutes) activity, MAX(log.create_time) date
-                FROM scoreboard_log log
-                JOIN scoreboard_users usr ON usr.id = log.user_id
-                GROUP by usr.username;
-            """
+            pass
 
+        query = f"""
+            SELECT usr.username, SUM(log.active_minutes) activity, MAX(log.create_time) date
+            FROM scoreboard_log log
+            JOIN scoreboard_users usr ON usr.id = log.user_id
+            {where_clause}
+            {group_clause}
+            ORDER BY activity desc
+        """
         result = self.fetch_all(query)
         user_list = []
         for dic in result:
